95f9cd6260ae78b24b3410d50c3b7147b0e8a945
deepesh-sr <220714100078@cutm.ac.in>
2025-10-26 16:02:23 +0530

    first commit :)

---
+++ week1-challenge/vault/programs/vault/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("22grD33JBxV7au5EB2Fc3y4etFkiUt5MwUGM8TMBad1w");
+
+#[program]
+pub mod vault {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
