58b57c9290529e3d84096078fd58c3ae896b506d
deepesh-sr <220714100078@cutm.ac.in>
2025-10-27 16:20:55 +0530

    delegation added

---
+++ magicblock_vrf/programs/magicblock_vrf/src/instructions/delegate.rs
@@ -0,0 +1,48 @@
+use anchor_lang::prelude::*;
+use ephemeral_rollups_sdk::{anchor::delegate,cpi::DelegateConfig};
+
+use crate::User;
+
+#[delegate]
+#[derive(Accounts)]
+
+pub struct Delegate<'info>{
+
+    #[account(mut)]
+    pub user : Signer<'info>,
+
+    #[account(
+        mut,
+        del,
+        seeds = [b"user", user.key().as_ref()],
+        bump = user_account_pda.bump
+    )]
+    pub user_account_pda : Account<'info,User>,
+
+    /// CHECK :
+    pub validator : UncheckedAccount<'info>,
+    pub system_program : Program<'info,System>
+}
+
+impl <'info> Delegate<'info> {
+
+    pub fn delegate(&mut self)->Result<()>{
+        
+        let pda_seeds : &[&[u8]] = &[
+            b"user",
+            &self.user.key.as_ref(),
+        ];
+
+
+        self.delegate_user_account_pda(
+            &self.user,
+            pda_seeds,
+            DelegateConfig {
+                validator: Some(self.validator.key()),
+                ..Default::default()
+            },
+        )?;
+        Ok(())
+    }
+}
+
--- magicblock_vrf/programs/magicblock_vrf/src/instructions/mod.rs
+++ magicblock_vrf/programs/magicblock_vrf/src/instructions/mod.rs
@@ -1,6 +1,8 @@
 pub mod initialize_user;
 pub mod close_user;
 pub mod update_user;
+pub mod delegate;
 pub use initialize_user::*;
 pub use close_user::*;
 pub use update_user::*;
+pub use delegate::*;
